25d893f2c5fbe8f9606637f6243dd09f33fa96da
Sharvinee Ragunatha Rao <shriyasharvinee@yahoo.com>
2026-03-09 00:57:35 -0500

    Add method say() to Person class

--- src/person.py
+++ src/person.py
@@ -1,6 +1,6 @@
 class Person:
     """A class to represent a person."""
-
+    
     def __init__(self,
                  name: str | None = None,
                  age: int | None = None,
@@ -9,9 +9,20 @@ class Person:
         self.name = name
         self.age = age
         self.gender = gender
-
+        
     def greet(self) -> None:
         """Do a simple greeting and introduction."""
+        self.say(f"Hello! My name is {self.name}.")
+
+    def say(self,
+            *args: object,
+            sep: str | None = " ",
+            end: str | None = "\n",
+            file=None,
+            flush: bool = False) -> None:
+        """Say a word, phrase, sentence or paragraph."""
+        print(*args, sep=sep, end=end, file=file, flush=flush)
+    
         print(f'Hello! My name is {self.name}.')
 
     def introduce(self) -> None:
@@ -22,3 +33,4 @@ class Person:
         if self.gender is not None:
             intro += f" I identify as {self.gender}."
         print(intro)
+    
